78e7ec78329ce0129d394e275b4e596431a93098
Rishabh7838 <https://github.com/Rishabh7838/>
2019-03-01 12:42:01 +0530

    First commit

---
+++ app/src/main/java/com/example/rish/detect_currency/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.rish.detect_currency;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
